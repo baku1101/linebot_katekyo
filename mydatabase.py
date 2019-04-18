5f16e588894111d1d778f0611c3f0e231f85a08c
watanabe <kouki.1101@outlook.jp>
2019-04-19 04:05:20 +0900

    ローカルに環境作って実行できるようになった

--- mydatabase.py
+++ mydatabase.py
@@ -10,29 +10,30 @@ import sys
 
 url = os.getenv('DATABASE_URL', None)
 if url is None:
-    print('set DATABASE_URL')
+    print('must set DATABASE_URL')
     sys.exit(1)
 
 con = psycopg2.connect(url)
 cur = con.cursor()
 
-# datetime型を直接データベースに格納できないためadapterとconverterが必要
-def adapt_dtime(dtime):
-    #datetime.datetime -> str
-    return dtime.strftime("%Y/%m/%d %H:%M")
-
-def convert_dtime(dtime_str):
-    # bytes -> datetime.time
-    # sqlite3の内部ではstrもbytesとして保存されているため，str.decode()を挟む
-    dtime = datetime.datetime.strptime(dtime_str,"%Y/%m/%d %H:%M")
-    return dtime
+## datetime型を直接データベースに格納できないためadapterとconverterが必要
+#def adapt_dtime(dtime):
+#    #datetime.datetime -> str
+#    return dtime.strftime("%Y/%m/%d %H:%M")
+#
+#def convert_dtime(dtime_str):
+#    # bytes -> datetime.time
+#    # sqlite3の内部ではstrもbytesとして保存されているため，str.decode()を挟む
+#    dtime = datetime.datetime.strptime(dtime_str,"%Y/%m/%d %H:%M")
+#    return dtime
 
 def CreateTable(usrname):
-    cur.execute("CREATE TABLE IF NOT EXISTS {}(year int, month int, start timestamp, end timestamp, id integer primary key)".format(usrname))
+    cur.execute("CREATE TABLE IF NOT EXISTS {} (year int, month int, start timestamp, finish timestamp, id serial primary key);".format(usrname))
 # primary keyの型はint でなく integer じゃないとauto incrementが適用されないので注意
 
 def DropTable(usrname):
     cur.execute("DROP TABLE IF EXISTS {}".format(usrname))
+    con.commit()
 
 def Start(usrname):
     CreateTable(usrname)
@@ -40,18 +41,20 @@ def Start(usrname):
         return False
     cur.execute("CREATE TABLE {}_tmp(start timestamp)".format(usrname))
     start = datetime.datetime.now()
-    cur.execute("INSERT INTO {}_tmp(start) values (?)".format(usrname), (adapt_dtime(start),))
+    cur.execute("INSERT INTO {}_tmp(start) values (%s)".format(usrname), (start,))
+    con.commit()
     return True
 
 # 月をまたぐものは未対応
-def End(usrname):
+def Finish(usrname):
     if not isExistTable(usrname + '_tmp'):
         return False
-    end = datetime.datetime.now()
+    finish = datetime.datetime.now()
     cur.execute("SELECT start FROM {}_tmp".format(usrname))
     start = cur.fetchone()[0]
-    cur.execute("INSERT INTO {}(year, month, start, end) VALUES (?,?,?,?)".format(usrname), (end.year, end.month, start, adapt_dtime(end)))
+    cur.execute("INSERT INTO {}(year, month, start, finish) VALUES (%s,%s,%s,%s)".format(usrname), (finish.year, finish.month, start, finish))
     cur.execute("DROP TABLE {}_tmp".format(usrname))
+    con.commit()
     return True
 
 def GetTableByMonth(usrname, year, month):
@@ -59,24 +62,27 @@ def GetTableByMonth(usrname, year, month):
     l = cur.fetchall()
     return l
 
-def InsertRow(usrname, startStr, endStr):
+def InsertRow(usrname, startStr, finishStr):
     start = datetime.datetime.strptime(startStr , "%Y/%m/%d %H:%M")
-    end = datetime.datetime.strptime(endStr , "%Y/%m/%d %H:%M")
-    cur.execute("INSERT INTO {}(year, month, start, end) VALUES (?,?,?,?)".format(usrname), (start.year, start.month, adapt_dtime(start), adapt_dtime(end)))
+    finish = datetime.datetime.strptime(finishStr , "%Y/%m/%d %H:%M")
+    cur.execute("INSERT INTO {}(year, month, start, finish) VALUES (%s,%s,%s,%s)".format(usrname), (start.year, start.month, start, finish))
+    con.commit()
 
 # 月単位でデータを削除
 def DeleteMonthData(usrname, year, month):
     cur.execute("DELETE FROM {} WHERE year = {} AND month = {}".format(usrname, year, month))
+    con.commit()
 
 # 直近の1列削除
 def DeleteRow(usrname):
     cur.execute("SELECT count(*) FROM {}".format(usrname))
     bottom = cur.fetchone()[0]
     cur.execute("DELETE FROM {} WHERE id = {}".format(usrname, bottom))
+    con.commit()
 
 # nameテーブルがあるかどうかを返す(tmpテーブルがあるかの確かめ用)
 def isExistTable(name):
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    cur.execute("SELECT relname FROM pg_stat_user_tables")
     l = cur.fetchall()
     if ((name,) in l):
         return True
@@ -87,7 +93,7 @@ def GetSumOfMonth(usrname, year, month):
     cur.execute("SELECT * FROM {} WHERE year = {} AND month = {}".format(usrname, year, month))
     sum_time = datetime.timedelta(0)
     for row in cur.fetchall():
-        sum_time += (convert_dtime(row[3]) - convert_dtime(row[2]))
+        sum_time += (row[3] - row[2])
     return sum_time
 
 if __name__ == '__main__':
@@ -95,4 +101,4 @@ if __name__ == '__main__':
     CreateTable(u)
     #Start(u)
     #time.sleep(1)
-    #End(u)
+    #finish(u)
--- test.py
+++ test.py
@@ -1,18 +1,24 @@
 import mydatabase
 import datetime
+import os
+import time
 
 if __name__ == '__main__':
     u = 'usr'
     t = datetime.datetime.now()
     mydatabase.CreateTable(u)
-    mydatabase.Start(u)
-    mydatabase.End(u)
+    if mydatabase.Start(u):
+        print("not started")
+    if mydatabase.Finish(u):
+        print("not finished")
+    print("start and finish: ",mydatabase.GetTableByMonth(u, t.year, t.month))
     mydatabase.InsertRow(u, '2019/4/1 10:00', '2019/4/1 12:00')
-    print(mydatabase.GetTableByMonth(u, t.year, t.month))
+    print("insert: ",mydatabase.GetTableByMonth(u, t.year, t.month))
     mydatabase.DeleteRow(u)
-    print(mydatabase.GetTableByMonth(u, t.year, t.month))
-    mydatabase.GetSumOfMonth(u, t.year, t.month)
+    print("delete: ",mydatabase.GetTableByMonth(u, t.year, t.month))
+    print("sum: ", mydatabase.GetSumOfMonth(u, t.year, t.month))
     l = mydatabase.GetTableByMonth(u, t.year, t.month)
     for row in l:
-        rd = datetime.datetime(row)
-        print(rd.strftime("%Y/%m/%d %H:%M"))
+        print(row)
+        #rd = datetime.datetime(row)
+        #print(rd.strftime("%Y/%m/%d %H:%M"))
